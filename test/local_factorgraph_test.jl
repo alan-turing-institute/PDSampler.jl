04459428ec2f5f3c06737e7e2fc64423ed00723f
Thibaut Lienart <ltib@me.com>
2017-04-07 16:49:10 +0100

    factorgraph coverage fix

--- test/local_factorgraph_test.jl
+++ test/local_factorgraph_test.jl
@@ -11,11 +11,28 @@ mvg            = PDMP.MvGaussianCanon(mu,P1)
 gll(x)         = PDMP.gradloglik(mvg, x)
 nextevent(x,v) = PDMP.nextevent_bps(mvg, x, v)
 
-chain = chaingraph([ Factor( nextevent, gll, i) for i in 1:3])
-compc = FactorGraphStruct([[1,2],[2,3],[3,4]])
+listfactors = [ Factor( nextevent, gll, i) for i in 1:3]
+chain       = chaingraph(listfactors)
+compc       = FactorGraphStruct([[1,2],[2,3],[3,4]])
+chain2      = FactorGraph(FactorGraphStruct([[1,2],[2,3],[3,4]]), listfactors)
+
+i = rand(1:3)
+x = randn(p)
+v = randn(p)
 
 @test   chain.structure.flist == compc.flist &&
         chain.structure.vlist == compc.vlist
+
+@test   chain2.structure.flist == chain.structure.flist &&
+        chain2.structure.vlist == chain.structure.vlist
+
+@test chain.factors[i].gll(x) == chain2.factors[i].gll(x)
+
+srand(12); a = chain2.factors[i].nextevent(x,v)
+srand(12); b = chain.factors[i].nextevent(x,v)
+
+@test a==b
+
 
 fgs = FactorGraphStruct( [[1,2,5],
                           [1,3,6],
