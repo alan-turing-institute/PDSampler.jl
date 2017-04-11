f3dc96caf7b957f2f825392872869d043e41f2d0
Thibaut Lienart <ltib@me.com>
2017-04-11 18:22:15 +0100

    test

--- docs/make.jl
+++ docs/make.jl
@@ -3,19 +3,8 @@ using Documenter, PDMP
 makedocs(
     modules  = [PDMP],
     doctest  = false,
-    format   = :html,
     sitename = "PDMP.jl",
-    authors  = "Thibaut Lienart",
-    pages    = Any[
-        "Home" => "index.md",
-        "Examples" => Any [
-            "examples/bps_mvg_constr.md",
-            "examples/lbps_gchain.md"
-        ],
-        "Technical Documentation" => Any [
-            "techdoc/types.md"
-        ]
-    ]
+    authors  = "Thibaut Lienart"
 )
 
 deploydocs(
