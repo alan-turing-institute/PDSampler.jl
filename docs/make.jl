bbe4173e398f7d5646e9ee3fee89fe1b1bde7bfe
Thibaut Lienart <ltib@me.com>
2017-05-10 13:06:28 +0100

    re-trying readthedocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,43 @@
 using Documenter, PDMP
 
 makedocs(
-    modules  = [PDMP],
-    doctest  = false,
-    clean    = true,
-    format   = :html,
+    modules = [PDMP],
+    format  = :html,
     sitename = "PDMP.jl",
-    authors  = "Thibaut Lienart"
+    pages   = [
+        "Home" => "index.md",
+        "Examples" => [
+            "Global BPS 1" => "examples/bps_mvg_constr.md",
+            "Local BPS 1" => "examples/lbps_gchain.md"
+        ]
+        "Technical doc" => [
+            "Types" => "techdoc/types.md"
+        ]
+    ]
 )
 
 deploydocs(
     repo   = "github.com/alan-turing-institute/PDMP.jl.git",
     target = "build",
-    osname = "linux",
-    branch = "gh-pages",
+    make   = "nothing",
     julia  = "0.5",
-    deps   = Deps.pip("mkdocs","python-markdown-math")
-    )
+    osname = "linux",
+    deps   = Deps.pip("mkdocs", "python-markdown-math"))
+
+# makedocs(
+#     modules  = [PDMP],
+#     doctest  = false,
+#     clean    = true,
+#     format   = :html,
+#     sitename = "PDMP.jl",
+#     authors  = "Thibaut Lienart"
+# )
+#
+# deploydocs(
+#     repo   = "github.com/alan-turing-institute/PDMP.jl.git",
+#     target = "build",
+#     osname = "linux",
+#     branch = "gh-pages",
+#     julia  = "0.5",
+#     deps   = Deps.pip("mkdocs","python-markdown-math")
+#     )
