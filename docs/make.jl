db1e3c9a81b5df48e7227596fa99660cbcaf4fc8
Thibaut Lienart <ltib@me.com>
2017-05-10 15:02:22 +0100

    I may eventually give up

--- docs/make.jl
+++ docs/make.jl
@@ -22,7 +22,7 @@ deploydocs(
     repo   = "github.com/alan-turing-institute/PDMP.jl.git",
     target = "build",
     branch = "gh-pages",
-    deps   = "nothing",
-    make   = "nothing",
+    julia  = "0.5",
+    osname = "linux",
     deps   = Deps.pip("pygments", "mkdocs", "python-markdown-math")
 )
