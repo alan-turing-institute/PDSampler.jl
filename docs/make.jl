0a3e70a0cf61cd022436685c6c5ec055dd26080a
Thibaut Lienart <ltib@me.com>
2017-05-10 16:13:36 +0100

    desperate attempt for docs then giving up

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,30 @@
 using Documenter, PDMP
 
-makedocs()
+makedocs(
+    modules = [PDMP],
+    clean = false,
+    format = :html,
+    sitename = "PDMP.jl",
+    authors = "Thibaut Lienart",
+    pages = Any[
+        "Introduction" => "index.md",
+        "Examples" => Any[
+            "Global BPS 1" => "examples/bps_mvg_constr.md",
+            "Local BPS 1" => "examples/lbps_gchain.md"
+        ],
+        "Technical Documentation" => Any[
+            "Types" => "types.md"
+        ]
+    ]
+)
 
 deploydocs(
     repo   = "github.com/alan-turing-institute/PDMP.jl.git",
     target = "build",
-    branch = "gh-pages",
     julia  = "0.5",
-    osname = "linux",
-    deps   = Deps.pip("pygments", "mkdocs", "python-markdown-math")
+    deps = nothing,
+    make = nothing
 )
+#    branch = "gh-pages",
+#    osname = "linux",
+#    deps   = Deps.pip("pygments", "mkdocs", "python-markdown-math")
