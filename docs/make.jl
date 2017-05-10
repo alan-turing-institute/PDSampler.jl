35db42c3f1c4c3c39187f1caebf61312c2587643
Thibaut Lienart <ltib@me.com>
2017-05-10 15:25:06 +0100

    it really wants a YML file

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,6 @@
 using Documenter, PDMP
 
-makedocs(
-    modules = [PDMP],
-    format  = :html,
-    sitename = "PDMP.jl",
-    authors = "Thibaut Lienart",
-    pages   = Any[
-        "Home"     => "index.md",
-        "Examples" => Any[
-            "Global BPS 1"  => "examples/bps_mvg_constr.md",
-            "Local BPS 1"   => "examples/lbps_gchain.md"
-        ],
-        "Technical doc" => [
-            "Types" => "techdoc/types.md"
-        ],
-    ],
-    doctest = false
-)
+makedocs()
 
 deploydocs(
     repo   = "github.com/alan-turing-institute/PDMP.jl.git",
