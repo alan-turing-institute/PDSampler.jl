b1e1c07740944d37522e062633ac080f8711ef26
tlienart <tlienart@me.com>
2018-02-23 19:08:05 +0000

    gaussian test OK

--- src/models/mvgaussian.jl
+++ src/models/mvgaussian.jl
@@ -15,12 +15,12 @@ and the covariance matrix. Users should prefer the Canonical representation if
 they can use that.
 """
 struct MvGaussianStandard <: MvGaussian
-    mu::Vector{AbstractFloat}  # Precision*mean
-    cov::Matrix{AbstractFloat} # negative Precision
+    mu::Vector{Real}  # Precision*mean
+    cov::Matrix{Real} # negative Precision
     p::Int # Dimensions
     #
-    prec::Matrix{AbstractFloat}
-    precmu::Vector{AbstractFloat}
+    prec::Matrix{Real}
+    precmu::Vector{Real}
     function MvGaussianStandard(mu, cov)
         # stored+considered as lower triangular
         L = cholfact(Symmetric(cov))[:L]
@@ -37,9 +37,9 @@ end
 Representation of a multivariate diagonal gaussian (diagonal covariance).
 """
 struct MvDiagonalGaussian <: MvGaussian
-    mu::Vector{AbstractFloat}     # mean
-    sigma::Vector{AbstractFloat}  # standard deviation
-    sigma2::Vector{AbstractFloat} # standard deviation squared
+    mu::Vector{Real}     # mean
+    sigma::Vector{Real}  # standard deviation
+    sigma2::Vector{Real} # standard deviation squared
     p::Int # dimensions
     MvDiagonalGaussian(mu, sigma) = new(mu, sigma, sigma.^2, length(mu))
 end
@@ -51,9 +51,9 @@ Canonical representation of a multivariate Gaussian instantiated from the mean
 and the precision matrix. This is the preferred representation.
 """
 struct MvGaussianCanon <: MvGaussian
-    mu::Vector{AbstractFloat}     # mean
-    prec::Matrix{AbstractFloat}   # Precision
-    precmu::Vector{AbstractFloat} # Precision*mean
+    mu::Vector{Real}     # mean
+    prec::Matrix{Real}   # Precision
+    precmu::Vector{Real} # Precision*mean
     p::Int # Dimensions
     function MvGaussianCanon(mu, prec)
         @assert length(mu) == size(prec, 1) == size(prec, 2)
@@ -67,13 +67,13 @@ end
 Natural parameter space representation of a multivariate Gaussian.
 """
 struct MvGaussianNatural <: MvGaussian
-    precmu::Vector{AbstractFloat}   # Precision*mean
-    negprec::Matrix{AbstractFloat}  # negative Precision
+    precmu::Vector{Real}   # Precision*mean
+    negprec::Matrix{Real}  # negative Precision
     p::Int # Dimensions
     MvGaussianNatural(precmu, negprec) = new(precmu, negprec, length(precmu))
 end
 
-# -----------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 ### Helper functions for efficient computations
 # the type aliases are not exposed
 
@@ -92,19 +92,19 @@ mvg_precmu(g::MvGN) = g.precmu
 mvg_precmu(g::MvGS) = g.precmu
 mvg_precmu(g::MvDG) = g.mu ./ g.sigma2
 
-mvg_precmult(g::MvGC, x::Vector{AbstractFloat}) =  g.prec * x
-mvg_precmult(g::MvGN, x::Vector{AbstractFloat}) = -g.negprec * x
-mvg_precmult(g::MvGS, x::Vector{AbstractFloat}) =  g.prec * x
-mvg_precmult(g::MvDG, x::Vector{AbstractFloat}) =  x ./ g.sigma2
+mvg_precmult(g::MvGC, x::Vector{<:Real}) =  g.prec * x
+mvg_precmult(g::MvGN, x::Vector{<:Real}) = -g.negprec * x
+mvg_precmult(g::MvGS, x::Vector{<:Real}) =  g.prec * x
+mvg_precmult(g::MvDG, x::Vector{<:Real}) =  x ./ g.sigma2
 
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 
 """
     gradloglik(g, w)
 
 Gradient of the loglikelihood of a multivariate Gaussian `g` at point `x`.
 """
-gradloglik(g::MvGaussian, x::Vector{AbstractFloat}) =
+gradloglik(g::MvGaussian, x::Vector{<:Real}) =
     mvg_precmu(g) - mvg_precmult(g, x)
 
 
@@ -113,5 +113,5 @@ gradloglik(g::MvGaussian, x::Vector{AbstractFloat}) =
 
 Loglikelihood of a multivariate Gaussian `g` at point `x`.
 """
-loglik(g::MvGaussian, x::Vector{AbstractFloat}) =
+loglik(g::MvGaussian, x::Vector{<:Real}) =
     0.5dot(mvg_precmu(g) - mvg_precmult(g, x), x - mvg_mu(g))
