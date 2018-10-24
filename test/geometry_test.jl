93d3844aaadc5b0c14b6b498eb133d54b2e866ff
Thibaut Lienart <tlienart@me.com>
2018-10-24 16:31:39 +1100

    fixes up to geometry

--- test/geometry_test.jl
+++ test/geometry_test.jl
@@ -1,20 +1,26 @@
 using PDSampler
-using Base.Test
+using Random
 
 # ------------------------------------------------------------------------------
 # Quick version of functions (for testing) (drawn from some of SJV's old code)
 function q_nextboundary(ns, a, x, v)
-    nconstr=length(a)
-    bts=a./(ns*v)-ns*x./(ns*v)
-    bts[Bool[ isapprox((ns*v)[i],0.0,atol=1.e-10)  for i=1:nconstr] ]=Inf #almost parallel
-    bts[Bool[ isapprox(bts[i],0.0,atol=1.e-10)  for i=1:nconstr] ]=Inf #almost parallel
+    nconstr = length(a)
+    bts = a./(ns*v) - ns*x./(ns*v)
+    mask1 = Bool[isapprox((ns*v)[i], 0.0, atol=1.e-10)  for i=1:nconstr]
+    mask2 = Bool[isapprox(bts[i], 0.0, atol=1.e-10)  for i=1:nconstr]
+    for (i, e) ∈ enumerate(mask1)
+        e && (bts[i] = Inf)
+    end
+    for (i, e) ∈ enumerate(mask2)
+        e && (bts[i] = Inf)
+    end
     bts+=(bts.<0.0)*Inf
     (t,j)=findmin(bts)
     (t,vec(ns[j,:]))
 end
 # ------------------------------------------------------------------------------
 
-srand(123)
+Random.seed!(123)
 
 p = 50
 c = 10
